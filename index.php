9b26360ffd0c0e9bb66249e8b93e292a136be68b
shun-sa <saku0aab@gmail.com>
2017-08-26 16:02:34 +0900

    add pagenation

--- index.php
+++ index.php
@@ -1,3 +1,126 @@
+<?php 
+
+function sqlite_open($location) 
+{ 
+    $handle = new SQLite3($location); 
+    return $handle; 
+} 
+
+function getCurrentPage()
+{
+    return (int) filter_input(INPUT_GET, 'page');
+}
+
+function asarray($str){
+	return explode(';', $str);
+	// try{return explode('[;]', $str);
+	// }catch(Exception $e){
+	// 	return $str;
+	// }
+}
+	
+
+function getobj($db_dir, $page = 0, $limit = null,  array $order = null){
+	#get database controller
+	$con=sqlite_open($db_dir);
+
+	#cuont all records
+	$record_num = $con->query("SELECT count(*) as count FROM titles")->fetchArray()["count"];
+
+	#set default sql request
+	$sql = "SELECT *  FROM titles";
+
+	#オーダーが指定されている時
+    if (!is_null($order)) {
+        $orderToken = [];
+        foreach ($order as $field => $value) {
+            $orderToken[] = sprintf('%s %s', $field, $value);
+        }
+        $orderStr = implode(', ', $orderToken);
+        $sql .= " ORDER BY {$orderStr}";
+    }
+
+    #limit数が指定されている時
+    if (!is_null($limit)) {
+        $offset = $page * $limit;
+        $sql .= sprintf(' LIMIT %d OFFSET %d', $limit, $offset);
+    }
+
+    $items = [];
+	$result = $con->query($sql);
+	while($row=$result->fetchArray()){
+		$items[] = $row;
+	}
+
+	return [ "obj" => $items, "recnum" => $record_num];
+
+}
+
+function pagination($recnum, $page, $limit){
+	$count = $recnum;
+
+    //レコード総数がゼロのときは何も出力しない
+    if (0 === $count) {
+        return '';
+    }
+
+    //現在表示中のページ番号（ゼロスタート）
+    $intCurrentPage = getCurrentPage();
+
+    //ページの最大数
+    $intMaxpage = ceil($count / $limit);
+
+    //現在ページの前後３ページを出力
+    $intStartpage = (2 < $intCurrentPage) ? $intCurrentPage - 3 : 0;
+    $intEndpage = (($intStartpage + 7) < $intMaxpage) ? $intStartpage + 7 : $intMaxpage;
+
+    //url組み立て
+    $urlparams = filter_input_array(INPUT_GET);
+
+    //表示中のページが先頭ではない時
+    if (0 < $intCurrentPage) {
+        $urlparams['page'] = $intCurrentPage - 1;
+        $items[] = sprintf('<li><a href="?%s">%s</a></li>'
+            , http_build_query($urlparams)
+            , '<'
+        );
+    }
+
+    for ($i = $intStartpage; $i < $intEndpage; $i++) {
+        $urlparams['page'] = $i;
+        $items[] = sprintf('<li%s><a href="?%s">%s</a></li>'
+            , ($intCurrentPage == $i) ? ' class="active"' : ''
+            , http_build_query($urlparams)
+            , $i + 1
+        );
+    }
+
+    //表示中のページが最後ではない時
+    if ($intCurrentPage < $intMaxpage and $limit<$recnum and $intCurrentPage!=$intMaxpage-1) {
+        $urlparams['page'] = $intCurrentPage + 1;
+        $items[] = sprintf('<li><a href="?%s">%s</a></li>'
+            , http_build_query($urlparams)
+            , '>'
+        );
+    }
+
+    return sprintf('<div class="pagination">%s</div>', implode(PHP_EOL, $items));
+}
+
+ini_set('display_errors',3000);
+error_reporting(E_ALL);
+
+$db_dir = "json2sqldata/database.db";
+$page = getCurrentPage();
+$limit = 30;
+$order = null;#['title' => 'asc', 'tag' => 'asc' ];
+
+$ret = getobj($db_dir, $page, $limit, $order);
+$obj = $ret["obj"];
+$recnum = $ret["recnum"];
+
+ ?>
+
 <!DOCTYPE html>
 <html lang="ja">
 	<head>
@@ -19,7 +142,7 @@
 		    </div>
 		    <ul class="nav navbar-nav">
 		      <li class="active"><a href="#">Home</a></li>
-		      <li><a href="#">Downloaded <span class="badge ">26</span></a></li>
+		      <!-- <li><a href="#">Downloaded <span class="badge ">26</span></a></li> -->
 		      <!-- <li valign="center"><a href="#"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span></a></li> -->
 
 		    </ul>
@@ -48,25 +171,25 @@
 		<dir class="container">
 			<h2>Title list</h2>
 				  <!-- <p>The .table-hover class enables a hover state on table rows:</p>             -->
-				  <table class="table table-hover ">
+				<table class="table table-hover ">
 					  <thead>
 					    <tr>
 					      <th>Title</th>
 					      <th>
-					      <a href="https://www.google.co.jp"></a>
+					      <a href="https://www.google.co.jp">tag</a>
 					      </th>
 					      <th>Air date</th>
-					      <th>Ep</th>
+					      <th>downloaded</th>
 					    </tr>
 					  </thead>
 					  <tbody>
 						<?php 
-						$jsonpath = "title.json";
-						echo file_exists($jsonUrl);
-						$json = file_get_contents($jsonpath);
-						$json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
-						$obj = json_decode($json,true);
-						$obj = $obj["titles"];
+						// $jsonpath = "title.json";
+						// // echo file_exists($jsonpath);
+						// $json = file_get_contents($jsonpath);
+						// $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+						// $obj = json_decode($json,true);
+						// $obj = $obj;
 						$taglist = [
 								    "drama" => "label-primary",
 								    "anime" => "label-success",
@@ -78,12 +201,12 @@
 							echo '<td width="70%">'.$val["title"]."</td>";
 					
 							echo "<td>";
-							foreach ($val["tag"] as $res){
+							foreach (asarray($val["tag"]) as $res){
 								echo '<span class="label '. $taglist[$res] .'">'.$res.'</span>&nbsp;';}
 							echo "</td>";
 
 							echo "<td>".$val["day"]."</td>";
-							echo '<td><span class="badge ">'.$val["story_num"]."</span></td>";
+							echo '<td align="center"><span class="badge ">'.$val["story_num"]."</span></td>";
 							echo "</tr>";
 						}
 					   ?>
@@ -91,8 +214,13 @@
 
 
 	  				  </tbody>
-					</table>
-		
+				</table>
+				<nav align="center">
+					<ul class="pagination">
+					<?= pagination($recnum, $page, $limit); ?>
+					</ul>
+				</nav>
+
 		</dir>
 
 
