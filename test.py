fefd00d48ff9f11e12dcc71075949dda19fca0f1
SAKURAI SHUN (櫻井 俊) <saku0aab@gmail.com>
2017-09-05 16:44:06 +0900

    fix add_pid method

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
 import time,os
 import json
 import signal
+from datetime import datetime
 
 class jobmng(object):
 	"""docstring for ClassName"""
-	def __init__(self):
-		pass
-		
+	def __init__(self,file_name):
+		self.file_name=file_name
+		self.reflesh()
+				
 	def pid_running_win(self, pid):
 	    import ctypes
 	    kernel32 = ctypes.windll.kernel32
@@ -27,7 +29,7 @@ class jobmng(object):
 	    else:
 	        return True
 
-	def check_pid(self, pid):        
+	def pid_exists(self, pid):        
 	    if os.name=="nt":return self.pid_running_win(pid)
 	    if os.name=="posix":return self.pid_running_unix(pid)
 
@@ -37,81 +39,81 @@ class jobmng(object):
 
 
 	def kill_process_win(self, pid):
-		if check_pid(pid):
+		if pid_exists(pid):
 			os.kill(pid, signal.SIGTERM)
 			return 0
 		else:
 			return 1
 
 	def kill_process_unix(self, pid):
-		if check_pid(pid):
+		if pid_exists(pid):
 			os.kill(pid, signal.SIGKILL)
 			return 0
 		else:
 			return 1
 
-	def kill_process(self, file_name,pid):
+	def kill_process(self, pid):
 		pid=int(pid)
-		self.remove_pid(file_name,pid,force=True)
+		self.remove_pid(self.file_name,pid,force=True)
 		if os.name=="nt":return kill_process_win(pid)
 		if os.name=="posix":return kill_process_unix(pid)
 
 
-	def load_json(self, file_name):
+	def load_json(self,):
 		try:
-			with open(file_name) as infile:
+			with open(self.file_name) as infile:
 				j = json.load(infile, object_hook=self.ascii_encode_dict)
 		except:
 			j={}
 		return j
 
-	def write_json(self, file_name,dic):
-		with open(file_name, 'w') as f:
+	def write_json(self ,dic):
+		with open(self.file_name, 'w') as f:
 		    json.dump(dic, f)
 
-	def reflesh(self, file_name):
-		j = self.load_json(file_name)
+	def reflesh(self, ):
+		j = self.load_json()
 		for i in j:
-			self.remove_pid(file_name,i)
+			self.remove_pid(i)
 
-	def killall(self, file_name):
-		j = self.load_json(file_name)
+	def killall(self, ):
+		j = self.load_json()
 		for i in j:
-			kill_process(file_name,i)
+			kill_process(i)
 
-	def add_pid(self, file_name,pid_dic):
-		self.reflesh(file_name)
-		
-		j=self.load_json(file_name)
-		j.update(pid_dic)
-		self.write_json(file_name,j)
+	def add_pid(self,pid,state):	
+		j=self.load_json()
+
+		try:
+			j["%s"%pid].update(state)			
+		except:
+			pid_dic={"%s"%pid:state}
+			j.update(pid_dic)
+
+		self.write_json(j)
 		return j
 
 
-	def remove_pid(self, file_name,pid,force=False):
+	def remove_pid(self,pid,force=False):
 		pid=int(pid)
-		if (os.path.exists(file_name) and not self.check_pid(pid)) or force:
-			j=self.load_json(file_name)
+		if (os.path.exists(self.file_name) and not self.pid_exists(pid)) or force:
+			j=self.load_json()
 			del j["%s"%pid]
-			self.write_json(file_name,j)
-			# print "#"
+			self.write_json(j)
+
+
 
 if __name__ == '__main__':
 	pid =os.getpid()
-	from datetime import datetime
-	date = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
 
-	pid_dic = {pid:{"flag" : True,"time":date}}
-	file_name="jobpid"
-	jm=jobmng()
-
-	jm.reflesh(file_name)
-	jm.add_pid(file_name,pid_dic)
+	state = {"flag" : True,"time":datetime.now().strftime("%Y/%m/%d %H:%M:%S")}
+	jm=jobmng("jobpid")
+	
+	jm.add_pid(pid,state)
+	# time.sleep(10)
 	for i in range(6):
 		print i
-		# f=open("test_%03d"%i,"w")
-		# f.close()
-		# print os.system("touch %03d"%i)
+		jm.add_pid(pid,{"status":i})
 		time.sleep(1)
 
-	jm.remove_pid(file_name,pid,force=True)
+	jm.remove_pid(pid,force=True)
