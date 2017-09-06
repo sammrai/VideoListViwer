a569505d3e7b73c9fcf3ce20103ed8edca1e0ed8
SAKURAI SHUN (櫻井 俊) <saku0aab@gmail.com>
2017-09-06 11:15:52 +0900

    fix footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,17 @@
-	<div class="container" id=footer>
-		<footer align="center">
-		<p class="text-muted"><a href="https://github.com/shun-sa/VideoListViwer">Github</a></p>
-		</footer>
+<div class="container" id=footer>
+	<div class="row">
+	<hr>
+
+	<div class="col-md-6">
+		<a class="text-muted"><a href="https://github.com/shun-sa/VideoListViwer">Github</a></a>
+		<a class="text-muted"><a href="#">About</a></a>
 	</div>
+
+	<div class="col-md-6">
+		<div align="right">
+
+		</div>
+	</div>
+	</div>
+</div>
+
